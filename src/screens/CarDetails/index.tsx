cf119bc1b456af8f912ff1eb8de674ec0f701df2
Manoel Martins <mano.martins29@gmail.com>
2022-02-12 20:50:29 -0300

    :gear: add more details

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -5,6 +5,15 @@ import {
   Container,
   Header,
   CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
 } from './styles';
 
 export function CarDetails() {
@@ -17,6 +26,25 @@ export function CarDetails() {
       <CarImages>
         <ImageSlider imagesUrl={['https://cdn.picpng.com/ferrari/ferrari-face-28762.png']} />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Ferrari</Brand>
+            <Name>490</Name>
+          </Description>
+
+          <Rent>
+             <Period>Ao dia</Period>
+             <Price>R$ 380,00</Price>
+          </Rent>
+        </Details>
+     
+        <About>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa velit ullam dolore rem, voluptas labore quas. Excepturi, illo, suscipit recusandae illum, reprehenderit optio ducimus libero itaque corrupti ut maxime?
+        </About>
+      </Content>
+
     </Container>
   );
 }
