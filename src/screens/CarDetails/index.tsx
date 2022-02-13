cce347aab69a4e88546a368d7f46d771472a1ca6
Manoel Martins <mano.martins29@gmail.com>
2022-02-12 21:14:01 -0300

    :sparkles: add accessory

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Accessory } from '../../components/Accessory';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
+
 import {
   Container,
   Header,
@@ -13,6 +22,7 @@ import {
   Rent,
   Period,
   Price,
+  Accessories,
   About,
 } from './styles';
 
@@ -39,6 +49,15 @@ export function CarDetails() {
              <Price>R$ 380,00</Price>
           </Rent>
         </Details>
+
+        <Accessories>
+          <Accessory name="370km/h" icon={speedSvg} />
+          <Accessory name="3.1s" icon={accelerationSvg} />
+          <Accessory name="750HP" icon={forceSvg} />
+          <Accessory name="Gasolina" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={exchangeSvg} />
+          <Accessory name="2 Pessoas" icon={peopleSvg} />
+         </Accessories>
      
         <About>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa velit ullam dolore rem, voluptas labore quas. Excepturi, illo, suscipit recusandae illum, reprehenderit optio ducimus libero itaque corrupti ut maxime?
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -66,10 +66,21 @@ export const Price = styled.Text`
   font-size: ${RFValue(25)}px;
 `
 
+export const Accessories = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 16px;
+`
+
 export const About = styled.Text`
   font-family: ${({ theme }) => theme.fonts.secondary_400};
   color: ${({ theme }) => theme.colors.text_detail};
   font-size: ${RFValue(15)}px;
   text-align: justify;
   margin-top: 24px; 
+  line-height: ${RFValue(25)}px; 
 `
